8e0c3a4d064730e09203c528ea9dc453d1b9fed8
Sumit <sumit.kumar3628@gmail.com>
2021-05-17 23:45:00 +0530

    uploading files

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+     return(
+        <nav>
+            <p className='smart f2 underline'>
+                Smart Raptor
+            </p>
+        </nav>
+    );
+}
+
+export default Navigation;
